5cfc740e6d444fc39d70203aa65d63a874203a7a
Qiang Zhao <koqizhao@outlook.com>
2018-06-19 18:20:04 +0800

    update version to 1.0.0

--- java/scf-labeled/src/main/java/org/mydotey/scf/labeled/AbstractLabeledConfigurationSource.java
+++ java/scf-labeled/src/main/java/org/mydotey/scf/labeled/AbstractLabeledConfigurationSource.java
@@ -1,12 +1,10 @@
 package org.mydotey.scf.labeled;
 
-import java.util.Collection;
 import java.util.Collections;
 
 import org.mydotey.scf.AbstractConfigurationSource;
 import org.mydotey.scf.ConfigurationSourceConfig;
 import org.mydotey.scf.PropertyConfig;
-import org.mydotey.scf.facade.ConfigurationProperties;
 
 /**
  * @author koqizhao
@@ -22,17 +20,7 @@ public abstract class AbstractLabeledConfigurationSource extends AbstractConfigu
 
     @Override
     public <K, V> V getPropertyValue(PropertyConfig<K, V> config) {
-        PropertyConfig<?, V> propertyConfig = config;
-        Collection<PropertyLabel> propertyLabels = Collections.<PropertyLabel> emptyList();
-        if (config.getKey() instanceof LabeledKey) {
-            propertyConfig = ConfigurationProperties.<Object, V> newConfigBuilder()
-                    .setKey(((LabeledKey<?>) config.getKey()).getKey()).setValueType(config.getValueType())
-                    .setDefaultValue(config.getDefaultValue()).addValueConverters(config.getValueConverters())
-                    .setValueFilter(config.getValueFilter()).build();
-            propertyLabels = ((LabeledKey<?>) config.getKey()).getLabels().getLabels();
-        }
-
-        return getPropertyValue(propertyConfig, propertyLabels);
+        return getPropertyValue(config, Collections.<PropertyLabel> emptyList());
     }
 
 }
